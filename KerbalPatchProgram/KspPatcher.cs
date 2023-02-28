a5138f16a61de29357b0f2346d7f8c16dfb9368b
Will Cobb <willlcobb@gmail.com>
2023-02-28 16:16:52 -0600

    Initial Commit

---
+++ KerbalPatchProgram/KspPatcher.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using BepInEx;
+using I2.Loc;
+using KSP.Game;
+using UnityEngine;
+using KSP.OAB;
+using KSP.Sim;
+using KSP.Sim.impl;
+using UnityEngine.SceneManagement;
+using Random = System.Random;
+
+namespace KSPTestMod
+{
+    // Attribute that contains information about the plugin. Required for the plugin to be loaded
+    [BepInPlugin("KerbalPatchProgram", "A collection of bug fixes for KSP2 bugs.", "0.1.0")]
+    public class KSPUtilsMod : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            var g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            Component.Destroy(g.GetComponent<SphereCollider>());
+            Component.Destroy(g.GetComponent<Rigidbody>());
+            g.transform.localScale = new Vector3(0, 0, 0);
+            g.AddComponent<KSPUtils>();
+        }
+    }
+
+    public class KSPUtils : KerbalMonoBehaviour
+    {
+
+        private GameInstance gameInstance = null;
+
+        void Start()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        public void Update()
+        {
+            if (gameInstance == null)
+            {
+                gameInstance = FindObjectOfType<GameInstance>();
+            }
+
+            if (gameInstance != null)
+            {
+                FixVesselSituation();
+            }
+        }
+
+        private void FixVesselSituation()
+        {
+            var vessel = Game.ViewController.GetActiveVehicle(true)?.GetSimVessel();
+            if(vessel==null)
+            {
+                return;
+            }
+
+            if (vessel.Landed && vessel.AltitudeFromTerrain > 50)
+            {
+                vessel.SimulationObject.objVesselBehavior.CheckSetLanded();
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+A collection of bug fixes for KSP2.
+
+Current Bug Fixes
+
+1. Prevent ships from getting stuck in a "Landed" state while flying. This bug prevents you from seeing your ship's trajectory in the map view.
+
+Install
+
+1. Download and install BepInEx 5
+2. Download and extract this mod into your KSP 2 install folder (usually "Kerbal Space Program 2")
+
